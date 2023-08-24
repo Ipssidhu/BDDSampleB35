a83339d532a4efa7f5e62a9134231cb1b9fb9f53
Administrator <Administrator@172.31.1.118>
2023-08-24 05:53:16 +0000

    Initial

---
+++ src/test/java/stepDefs/GooglePageStepDef.java
@@ -0,0 +1,36 @@
+package stepDefs;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GooglePageStepDef {
+	WebDriver driver ;
+	
+	@Given("User is on Google Page")
+	public void user_is_on_google_page() {			
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");   
+	}
+	
+	@When("I search Java Tutorial")
+	public void i_search_java_tutorial() {
+		WebElement srcBox= driver.findElement(By.name("q"));
+		srcBox.sendKeys("Java Tutorial");
+		srcBox.submit();
+	}
+	
+	@Then("Should display Java result page")
+	public void should_display_java_result_page() {
+		String strTitle = driver.getTitle(); 
+		Assert.assertEquals(strTitle, "Java Tutorial - Google Search ");
+	}
+}
